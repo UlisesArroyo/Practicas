ab2d6f9922936cac209fd601bc68687eeb11eac2
UlisesArroyo <66101708+UlisesArroyo@users.noreply.github.com>
2020-06-01 02:00:12 -0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,16 @@
+import cv2
+import torch
+from torchvision import transforms
+from PIL import Image 
+
+transforms = transforms.Compose([
+	transforms.ToTensor()])
+
+# Cargamos la imagen del disco duro
+imagen = cv2.imread("dog.jpg")
+cv2.imshow("prueba", imagen)
+cv2.waitKey(0)
+
+img = Image.open("dog.jpg")
+img_t = transforms(img)
+img_gpu = torch.tensor(img_t, device = 'cpu')
